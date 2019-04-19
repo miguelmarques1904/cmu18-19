633addb378c942f18a07bd62b1af1533de393763
Miguel Marques <migsmarques@gmail.com>
2019-04-20 00:28:24 +0100

    Basic button functionality

--- app/src/main/java/pt/ist/cmu/p2photo/MainActivity.java
+++ app/src/main/java/pt/ist/cmu/p2photo/MainActivity.java
@@ -2,6 +2,8 @@ package pt.ist.cmu.p2photo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void signUpButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
+    public void logInButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
+    public void createAlbumButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
+    public void findUserButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
+    public void addPhotoButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
+    public void addUserButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
+
+    public void listUserButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
+    public void viewAlbumButtonOnClick(View v) {
+        Button button=(Button) v;
+        button.setText("top");
+    }
+
 }
